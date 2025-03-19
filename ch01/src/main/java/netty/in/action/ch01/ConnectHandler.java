6a5f4a22f4736120fa56627aa75e152fc1013cc4
Giwan Kim <giwan.kim@gmail.com>
2025-03-19 12:34:41 +0900

    feat: add example of a Netty callback for handling new connection established event

---
+++ ch01/src/main/java/netty/in/action/ch01/ConnectHandler.java
@@ -0,0 +1,11 @@
+package netty.in.action.ch01;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ConnectHandler extends ChannelInboundHandlerAdapter {
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    System.out.println("Client " + ctx.channel().remoteAddress() + " connected");
+  }
+}
