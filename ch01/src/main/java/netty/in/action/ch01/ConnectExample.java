4d5d7df83fcbcb8f996752c898e7f86dee174478
Giwan Kim <giwan.kim@gmail.com>
2025-03-19 19:09:51 +0900

    feat: add ChannelFuture example

--- ch01/src/main/java/netty/in/action/ch01/ConnectExample.java
+++ ch01/src/main/java/netty/in/action/ch01/ConnectExample.java
@@ -6,7 +6,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.socket.nio.NioSocketChannel;
-
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
 
@@ -16,18 +15,19 @@ public class ConnectExample {
   public static void connect() {
     Channel channel = CHANNEL_FROM_SOMEWHERE;
     ChannelFuture future = channel.connect(new InetSocketAddress("192.168.0.1", 25));
-    future.addListener(new ChannelFutureListener() {
-      @Override
-      public void operationComplete(ChannelFuture future) throws Exception {
-        if (future.isSuccess()) {
-          ByteBuf buffer = Unpooled.copiedBuffer("Hello", Charset.defaultCharset());
-          ChannelFuture wf = future.channel().writeAndFlush(buffer);
-          // ...
-        } else {
-          Throwable cause = future.cause();
-          cause.printStackTrace();
-        }
-      }
-    });
+    future.addListener(
+        new ChannelFutureListener() {
+          @Override
+          public void operationComplete(ChannelFuture future) throws Exception {
+            if (future.isSuccess()) {
+              ByteBuf buffer = Unpooled.copiedBuffer("Hello", Charset.defaultCharset());
+              ChannelFuture wf = future.channel().writeAndFlush(buffer);
+              // ...
+            } else {
+              Throwable cause = future.cause();
+              cause.printStackTrace();
+            }
+          }
+        });
   }
 }
